cdad5d9dd83f8a4f47d139d63e82d20f4201e001
Juan Munoz <juanmunoz890@gmail.com>
2025-04-07 13:53:22 -0300

    update protos - add timestamp field

--- eigenda-v2-proto-bindings/build.rs
+++ eigenda-v2-proto-bindings/build.rs
@@ -1,15 +1,15 @@
 use std::io::Result;
 fn main() -> Result<()> {
-    prost_build::compile_protos(
-        &[
-            // the common proto is ommited because it already is a
-            // dependency of the other protos
-            "src/proto/disperser/v2/disperser_v2.proto",
-            "src/proto/retriever/v2/retriever_v2.proto",
-            "src/proto/encoder/v2/encoder.proto",
-            "src/proto/validator/node_v2.proto",
-        ],
-        &["src/proto"],
+    tonic_build::configure().compile(
+            &[
+                // the common proto is ommited because it already is a
+                // dependency of the other protos
+                "src/proto/disperser/v2/disperser_v2.proto",
+                "src/proto/retriever/v2/retriever_v2.proto",
+                "src/proto/encoder/v2/encoder.proto",
+                "src/proto/validator/node_v2.proto",
+            ],
+            &["src/proto"],
     )?;
     Ok(())
 }
